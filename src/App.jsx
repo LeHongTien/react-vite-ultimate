00bedbfca1274011f42a6405f7e9e3101c4c9167
lehongtien <lehongtien@gmail.com>
2025-02-14 13:38:41 +0700

    video 28

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const MyComponent = () => {
+  return (
+    <div>eric & hoidanit update</div>
+  );
+}
+
 const App = () =>  {
   const [count, setCount] = useState(0)
 
-  const myFunction = () => {
-    console.log(">>> run my arrow function");
-  }
-
-  myFunction();
-  
   return (
     <>
       <div>
@@ -23,6 +23,7 @@ const App = () =>  {
         </a>
       </div>
       <h1>Hello World & hoidanit</h1>
+      <MyComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
