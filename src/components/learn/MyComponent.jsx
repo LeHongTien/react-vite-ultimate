810dc7a55b0106fe17fe61dc9483ab127ffa90a9
lehongtien <lehongtien@gmail.com>
2025-02-14 14:21:53 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,13 @@
 import "./style.css";
+
 const MyComponent = () => {
+    // const hoidanit = "eric";
+    // const hoidanit = 25;
+    const hoidanit = [1,2,3]
     return (
       <>
-        <div>eric & hoidanit update</div>
+        <div>{JSON.stringify(hoidanit)} & hoidanit update</div>
+        <div>{console.log("ERIC")}</div>
         <div className="child"
         style={
             {borderRadius: "10px"}}
