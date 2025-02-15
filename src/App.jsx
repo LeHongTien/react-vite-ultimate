565d487ee976d6f8178a71efe32389064d3cecfe
lehongtien <lehongtien@gmail.com>
2025-02-15 22:08:49 +0700

    video 42

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import { useState } from 'react'
 
 const App = () =>  {
   const [todoList, setTodoList] = useState([
-    // { id: 1, name: "Learning React" },
-    // { id: 2, name: "Watching Youtube" }
   ]);
  
   const addNewTodo = (name) => {
@@ -17,10 +15,14 @@ const App = () =>  {
     }
     setTodoList([...todoList, newTodo])
   }
-  const randomIntFromInterval = (min, max) => { // min and max included 
+  const randomIntFromInterval = (min, max) => { 
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
   
+  const deleteTodo = (id) => {
+    setTodoList(todoList.filter((item) => item.id !== id))
+  }
+
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
@@ -30,22 +32,13 @@ const App = () =>  {
       {todoList.length > 0 ?
         <TodoData
           todoList={todoList}
+          deleteTodo={deleteTodo}
         />
         :
         <div className='todo-image'>
           <img src={reactLogo} className='logo' />
         </div>
       }
-      {/* {todoList.length > 0 && 
-      <TodoData
-        todoList={todoList}
-        />
-      }
-      {todoList.length === 0 &&
-      <div className='todo-image'>
-        <img src={reactLogo} className='logo'/>
-        </div>
-      } */}
     </div>
   )
 }
