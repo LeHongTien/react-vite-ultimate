0ace6b20808dd5f495019446e325a42cb85aed58
lehongtien <lehongtien@gmail.com>
2025-02-18 15:24:46 +0700

    video 80

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,8 +1,9 @@
 import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
-import { Table } from 'antd';
+import { notification, Popconfirm, Table } from 'antd';
 import UpdateUserModal from './update.user.modal';
 import { useState } from 'react';
 import ViewUserDetail from './view.user.detail';
+import { deleteUserAPI } from '../../services/api.service';
 
 const UserTable = (props) => {
   const { dataUsers, loadUser} = props;
@@ -48,13 +49,39 @@ const UserTable = (props) => {
                 setIsModalUpdateOpen(true)
               }}
               style={{ cursor: 'pointer', color: 'orange' }} />
-            <DeleteOutlined style={{ cursor: 'pointer', color: 'red'}}/>
+            <Popconfirm
+              title="Xoá người dùng"
+              description="Bạn chắc chắn xóa user này?"
+              onConfirm={() => {
+                handleDeleteUser(record._id)
+              }}
+              okText="Yes"
+              cancelText="No"
+              placement="left"
+            >
+              <DeleteOutlined style={{ cursor: 'pointer', color: 'red'}}/>
+            </Popconfirm>
           </div>
         </>
       ),
     },
   ];
   
+  const handleDeleteUser = async (id) => {
+    const res = await deleteUserAPI(id)
+    if (res.data) {
+      notification.success({
+        message: "Delete user",
+        description: "Xoá user thành công"
+      })
+      await loadUser();
+    } else {
+      notification.error({
+        message: "Error delete user",
+        description: JSON.stringify(res.message)
+      })
+    }
+  }
   return (
     <>
       <Table
