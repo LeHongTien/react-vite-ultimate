6e84bc171a973d85729fffa049cdff47d01da996
lehongtien <lehongtien@gmail.com>
2025-02-22 10:43:42 +0700

    video 105

--- src/pages/book.jsx
+++ src/pages/book.jsx
@@ -1,7 +1,12 @@
+import BookTable from "../components/book/book.table";
+
 const BookPage = () => {
     return (
-        <div>book page</div>
-    );
+        <div style={{ padding: "20px" }}>
+            <BookTable />
+        </div>
+
+    )
 }
 
-export default BookPage;
+export default BookPage
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -85,6 +85,11 @@ const logoutAPI = () => {
     const URL_BACKEND = "/api/v1/auth/logout";
     return axios.post(URL_BACKEND)
 }
+
+const fetchAllBookAPI = (current, pageSize) => {
+    const URL_BACKEND = `/api/v1/book?current=${current}&pageSize=${pageSize}`
+    return axios.get(URL_BACKEND);
+}
 export {
     createUserAPI,
     updateUserAPI,
@@ -95,5 +100,6 @@ export {
     registerUserAPI,
     loginAPI,
     getAccountAPI,
-    logoutAPI
+    logoutAPI,
+    fetchAllBookAPI
 }
